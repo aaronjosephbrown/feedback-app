639f60e17597de3e16e416e55fad1b6b4bd85006
Aaron J. Brown <39014977+aaronjosephbrown@users.noreply.github.com>
2023-02-18 20:33:12 -0600

    Updated Routes

--- src/App.js
+++ src/App.js
@@ -4,27 +4,47 @@ import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import AboutIconLink from './components/AboutIconLink'
+import About from './pages/About'
 
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData)
 
   const handleDelete = (id) => {
-    if(!window.confirm('Are you sure you want to delete the feedback?')) return
+    if (!window.confirm('Are you sure you want to delete the feedback?')) return
     const newFeedback = feedback.filter((item) => item.id !== id)
     setFeedback(newFeedback)
   }
   const addFeedback = (newFeedback) => {
-    setFeedback([newFeedback,...feedback])
+    setFeedback([newFeedback, ...feedback])
   }
+
+  const Root = [
+    <FeedbackForm handleAdd={addFeedback} />,
+    <FeedbackStats feedback={feedback} />,
+    <FeedbackList feedback={feedback} handleDelete={handleDelete} />,
+    <AboutIconLink />,
+  ]
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: Root.map((item, index) => <div key={index}>{item}</div>),
+    },
+    {
+      path: '/about',
+      element: <About />,
+    },
+  ])
 
   return (
     <>
       <Header />
       <div className='container'>
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={handleDelete} />
+        <RouterProvider router={router} />
       </div>
+      
     </>
   )
 }
---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+const About = () => {
+  return (
+    <div>
+      This is a React app that allows users to give feedback on a product. <br/>
+      <Link to='/'>Go back</Link>
+    </div>
+  )
+}
+
+export default About
